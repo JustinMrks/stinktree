6ef5580526543b8ff6a4426002893515b7000c88
justinmrks <justinmarks19@gmail.com>
2022-12-21 15:56:47 -0600

    added header with menu and editing toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Header from './Components/Header';
 
 import './App.css';
 import UserCard from './Components/UserCard';
 
 function App() {
+  const [editing, setEditing] = useState(false);
+  const [burgerMenu, setBurgerMenu] = useState(false);
+
   return (
     <div className="App">
-      <header>Header</header> {/* soon to be menu bar */}
+      <Header
+        editing={editing}
+        setEditing={setEditing}
+        burgerMenu={burgerMenu}
+        setBurgerMenu={setBurgerMenu}
+      />
       <Routes>
         <Route path="/user/:username" element={<UserCard />} />
       </Routes>
       <footer>Footer</footer>
-      {/* for holding contact info and icon contributors */}
     </div>
   );
 }
-
-/* <a href="https://iconscout.com/icons/instagram" target="_blank">Instagram Icon</a> by <a href="https://iconscout.com/contributors/wooof-studio" target="_blank">Wooof Studio</a> */
 
 export default App;
---
+++ src/Components/Header.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+type HeaderState = {
+  editing: boolean;
+  setEditing: React.Dispatch<React.SetStateAction<boolean>>;
+  burgerMenu: boolean;
+  setBurgerMenu: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Header = ({
+  editing,
+  setEditing,
+  burgerMenu,
+  setBurgerMenu,
+}: HeaderState) => {
+  return (
+    <div className="header">
+      <nav>
+        <button
+          className="nav-opener"
+          onClick={() => setBurgerMenu(!burgerMenu)}
+        >
+          =
+        </button>
+        {burgerMenu ? (
+          <div className="aria">
+            <Link
+              className="justin"
+              to="/user/JustinMarksTheSpot"
+              onClick={() => setBurgerMenu(!burgerMenu)}
+            >
+              Justin
+            </Link>
+            <Link
+              className="justin"
+              to="/user/WillWearingOfficial"
+              onClick={() => setBurgerMenu(!burgerMenu)}
+            >
+              Will
+            </Link>
+            <Link
+              className="justin"
+              to="/user/Mikeyboy"
+              onClick={() => setBurgerMenu(!burgerMenu)}
+            >
+              Mike
+            </Link>
+
+            <button onClick={() => setEditing(!editing)}>Edit Mode</button>
+          </div>
+        ) : (
+          <></>
+        )}
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
